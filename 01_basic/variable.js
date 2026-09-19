746c5029114388c578e23c6448b7ba6e5796a86d
kajal05963 <kajalraju.0506@gmail.com>
2026-09-19 13:58:28 +0000

    variables

---
+++ 01_basic/variable.js
@@ -0,0 +1,7 @@
+const accountID = "114433"
+let accountEmail = "kajalraju.0506@gamil.com"
+var accountPassaword = "87654"
+accountCity = "jaipur"
+
+//accountID =2
+console.table([accountID,accountEmail,accountPassaword,accountCity])
