b29f425fe840e55044990b9a28fdfc1a31912e51
kajal05963 <kajalraju.0506@gmail.com>
2026-09-19 17:14:31 +0000

    Convergen

---
+++ 01_basic/Convergen.js
@@ -0,0 +1,12 @@
+let a = "100";
+let b = 20;
+
+// Convert string to number
+console.log(Number(a) + b); // 120
+
+// Convert number to string
+console.log(a + String(b)); // "10020"
+
+// Boolean conversion
+console.log(Boolean(""));   // false
+console.log(Boolean("JS")); // true
