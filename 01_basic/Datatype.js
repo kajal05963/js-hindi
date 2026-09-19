d75bb8fc93a139b81eefdee15f9d1cc7bffa4e12
kajal05963 <kajalraju.0506@gmail.com>
2026-09-19 16:59:32 +0000

    datatypes

---
+++ 01_basic/Datatype.js
@@ -0,0 +1,40 @@
+// String - text data
+let name = "Kajal";
+console.log("String:", name);
+
+// Number - integer or decimal
+let age = 22;
+let pi = 3.14;
+console.log("Number:", age, pi);
+
+// Boolean - true/false
+let isStudent = true;
+console.log("Boolean:", isStudent);
+
+// Undefined - variable declared but not assigned
+let city;
+console.log("Undefined:", city);
+
+// Null - intentional empty value
+let car = null;
+console.log("Null:", car);
+
+// Object - key-value pairs
+let person = {
+    firstName: "Kajal",
+    lastName: "Singh",
+    age: 22
+};
+console.log("Object:", person);
+
+// Array - collection of values
+let colors = ["red", "green", "blue"];
+console.log("Array:", colors);
+
+// Symbol - unique identifier
+let id = Symbol("id");
+console.log("Symbol:", id);
+
+// BigInt - very large numbers
+let bigNumber = 123456789012345678901234567890n;
+console.log("BigInt:", bigNumber);
